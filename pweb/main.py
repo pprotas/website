a6e9bbb661652f8ade25cf52dde3e47ed5d302df
Pawel Protas <pawprotas@gmail.com>
2019-04-17 21:17:15 +0200

    Flask directory structured

---
+++ pweb/main.py
@@ -0,0 +1,24 @@
+from flask import (Blueprint, redirect, render_template)
+
+
+bp = Blueprint('main', __name__)
+
+
+@bp.route('/')
+def index():
+    return render_template('site/index.html')
+
+
+@bp.route('/about')
+def about():
+    return render_template('site/about.html')
+
+
+@bp.route('/portfolio')
+def portfolio():
+    return render_template('site/portfolio.html')
+
+
+@bp.route('/contact')
+def contact():
+    return render_template('site/contact.html')
